50910a229d51572aab1e3db43e70d44d1f3324ef
Bluemi <bruno.schilling@protonmail.ch>
2020-07-23 11:18:17 +0200

    removed Visualizer class

--- src/visualizer.hpp
+++ src/visualizer.hpp
@@ -8,41 +8,6 @@
 #include "shader_program.hpp"
 
 namespace visualizer {
-	class Creation;
-
-	class Visualizer {
-		public:
-			Visualizer(GLFWwindow* window, ShaderProgram shader_program, unsigned int window_width, unsigned int window_height);
-			Visualizer(const Visualizer& visu);
-			~Visualizer();
-
-			static void init();
-			static std::optional<Visualizer> create(unsigned int window_width, unsigned int window_height, const std::string& window_name);
-
-			void tick();
-			void render();
-			void close();
-			bool should_close() const;
-
-			double get_time() const;
-
-			void framebuffer_size_callback(GLFWwindow*, int width, int height);
-		private:
-			double get_delta_time();
-			void clear_window();
-
-			Controller _controller;
-			Camera _camera;
-			ShaderProgram _shader_program;
-
-			GLFWwindow* _window;
-
-			double _last_frame_time;
-
-			unsigned int _window_width;
-			unsigned int _window_height;
-	};
-
 	void init();
 	void clear_window();
 	void close();
